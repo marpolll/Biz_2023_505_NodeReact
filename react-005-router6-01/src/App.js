baee2c07a2b8c6b01f7c054beaf7ee073d531a2b
marpolll <minstarsoso@gmail.com>
2023-08-17 12:53:37 +0900

    router

---
+++ react-005-router6-01/src/App.js
@@ -0,0 +1,51 @@
+import logo from "./logo.svg";
+import "./css/App.css";
+import { useRoutes, Outlet } from "react-router-dom";
+import BBsMain from "./comps/BBsMain";
+import { Button } from "./styled/BBsStyled";
+
+/*
+  App Component를  
+  react-router-dom v6 에서 useRoutes() Hooks 함수를 생성하고
+  router 를 return 하는 구조로 변경한다.
+*/
+function App() {
+  // App Component 내에 inner Component 를 생성하기
+  const AppBody = () => {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+        </header>
+        <Outlet />
+      </div>
+    );
+  }; // end AppBody
+  // App.js Component 를 화면에 표현하면, react-router-dom 에 의해서 path를 감지하고
+  // AppBody Component 를 화면에 랜더링 한다..
+  const appRouter = useRoutes([
+    {
+      path: "/",
+      element: (
+        <>
+          {" "}
+          <AppBody />
+        </>
+      ),
+      children: [
+        {
+          path: "",
+          element: (
+            <Button bgColor="red" color="black">
+              게시판 열기
+            </Button>
+          ),
+        },
+        { path: "bbs/*", element: <BBsMain /> },
+      ],
+    },
+  ]);
+  return appRouter;
+}
+
+export default App;
---
+++ react-005-router6-01/src/comps/BBsList.jsx
@@ -0,0 +1,43 @@
+import { Table } from "../styled/BBsStyled";
+import BBsItem from "./BBsItem";
+
+/**
+ * 원래 제공되는 react comps 인 table 에 style 를 적용하여
+ * 새로운 Component 로 재 생성하기
+ */
+
+// const Table = styled.table`
+//   width: 80%;
+//   margin: 0px auto;
+//   border-collapse: collapse;
+//   border: 1px solid #ddd;
+//   & > .list-tr {
+//     border-bottom: 1px solid #ddd;
+//   }
+//   & th {
+//     padding: 5px;
+//     background-color: #aaa;
+//   }
+// `;
+
+const BBsList = ({ bbsListData }) => {
+  const bbsListItemView = bbsListData.map((item) => {
+    return <BBsItem item={item} key={item.id} />;
+  });
+  return (
+    <Table>
+      <thead>
+        <tr className="list-tr">
+          <th>No</th>
+          <th>제목</th>
+          <th>작성일자</th>
+          <th>작성자</th>
+          <th>조회수</th>
+        </tr>
+      </thead>
+      <tbody>bbsListItemView</tbody>
+    </Table>
+  );
+};
+
+export default BBsList;
